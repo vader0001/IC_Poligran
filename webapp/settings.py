6daf0b2ee47b9eae0338feb1fa3896b5727e2c68
unknown <eldeveloperjuan@gmail.com>
2021-06-29 19:21:21 -0500

    configurar sentry-sdk

--- webapp/settings.py
+++ webapp/settings.py
@@ -11,6 +11,22 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://086632946d20488993c0cb6902023e51@o82723.ingest.sentry.io/5840015",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- webapp/tests.py
+++ webapp/tests.py
@@ -1,10 +1,14 @@
 from django.test import TestCase
 from .models import Item
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+    
 #TODO fabricar tests unitarios 
 class ItemTestCase(TestCase):
 
     def test_Items_can_speak(self):
         """Items that can speak are correctly identified"""
         self.assertEqual(1,1)
+        trigger_error()
         
